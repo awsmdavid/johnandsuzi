94be483159ddd3c9993b9016c1754674e715caff
awsmdavid <awsmdavid@gmail.com>
2015-05-01 19:13:49 -0400

    sweet fadez, bro

--- js/app.js
+++ js/app.js
@@ -3,13 +3,37 @@
 $(document).foundation();
 $(document).ready(function(){
 	// alert("yo");
-	var divs = $('.welcome-content');
+	var welcomediv = $('.welcome-content');
+	var storydiv = $('.story-content');
+	var weddingdiv = $('.wedding-content');
+
 	$(window).scroll(function(){
 		if($(window).scrollTop()<500){
-			divs.fadeOut("fast");
+			welcomediv.fadeOut("fast");
 		}
 		else {
-			divs.fadeIn("slow");
+			welcomediv.fadeIn("slow");
 		}
+
+	});
+
+	$(window).scroll(function(){
+		if($(window).scrollTop()<1000){
+			storydiv.fadeOut("fast");
+		}
+		else {
+			storydiv.fadeIn("slow");
+		}
+		
+	});
+
+	$(window).scroll(function(){
+		if($(window).scrollTop()<1500){
+			weddingdiv.fadeOut("fast");
+		}
+		else {
+			weddingdiv.fadeIn("slow");
+		}
+		
 	});
 });
