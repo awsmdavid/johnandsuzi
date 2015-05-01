542bad14330600d906cc4989ef5c0ba63886021f
awsmdavid <awsmdavid@gmail.com>
2015-05-01 17:14:24 -0400

    sweet fade, bro

--- js/app.js
+++ js/app.js
@@ -3,5 +3,13 @@
 $(document).foundation();
 $(document).ready(function(){
 	// alert("yo");
-
+	var divs = $('.welcome-content');
+	$(window).scroll(function(){
+		if($(window).scrollTop()<500){
+			divs.fadeOut("fast");
+		}
+		else {
+			divs.fadeIn("slow");
+		}
+	});
 });
