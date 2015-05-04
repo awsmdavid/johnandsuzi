fb54940483eb026614e901278ad21c010ec67002
awsmdavid <awsmdavid@gmail.com>
2015-05-04 15:54:06 -0400

    fixed js

--- js/app.js
+++ js/app.js
@@ -2,33 +2,39 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 $(document).ready(function(){
+	// $('#arrow').show().animate({ top: 305 }, {duration: 1000, easing: 'easeOutBounce'});
 	// alert("yo");
+	// $('.nav-arrow').animate({ top: 305 }, {duration: 1000, easing: 'easeOutBounce'});
 	var welcomediv = $('.welcome-content');
 	var storydiv = $('.story-content');
 	var weddingdiv = $('.wedding-content');
 
 	$(window).scroll(function(){
-		if($(window).scrollTop()<500){
-			welcomediv.fadeOut("fast");
+		if($(window).scrollTop()>500){
+			welcomediv.fadeIn("slow");
 		}
 		else {
-			welcomediv.fadeIn("slow");
+			// $( ".story-content" ).animate({marginLeft: "0px"}, 500);
+			welcomediv.fadeOut("fast");
 		}
 
 	});
 
 	$(window).scroll(function(){
-		if($(window).scrollTop()<1000){
-			storydiv.fadeOut("fast");
+		if($(window).scrollTop()>800){
+			storydiv.fadeIn("slow");
+			// $( ".story-content" ).fadeIn("slow").animate({marginLeft: "50px"}, 500);
+
+			// $( ".story-content" ).animate({marginLeft: "50px"}, 500);
 		}
 		else {
-			storydiv.fadeIn("slow");
+			storydiv.fadeOut("fast");
 		}
 		
 	});
 
 	$(window).scroll(function(){
-		if($(window).scrollTop()<1500){
+		if($(window).scrollTop()<1300){
 			weddingdiv.fadeOut("fast");
 		}
 		else {
