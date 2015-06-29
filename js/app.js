b8915ff187c087263626083c0aedba45052058d2
awsmdavid <awsmdavid@gmail.com>
2015-06-28 21:22:31 -0400

    updated with firefly and party content

--- js/app.js
+++ js/app.js
@@ -1,19 +1,44 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+/*
+function doBounce(times, distance, speed) {
+	for(var i = 0; i < times; i++) {
+		$('#arrow').animate({marginTop: '-='+distance}, speed)
+		.animate({marginTop: '+='+distance}, speed);
+	}
+}
+*/
+
 $(document).ready(function(){
+
 	// Check if desktop
     if (/Android|BlackBerry|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent) === false) {
+		
+		// doBounce(3, '10px', 300);
+		$('#arrow').hover(function(){
+			$('#arrow').css('cursor','pointer');
+		});
+		$("#arrow").click(function() {
+			$('#arrow').hide();
+			$('html, body').animate({
+				scrollTop: $("#welcome").offset().top-100
+			}, 2000, function(){
+				$('#arrow').show();
+			});
+		});
+
 
 		// $('#arrow').show().animate({ top: 305 }, {duration: 1000, easing: 'easeOutBounce'});
 		// alert("yo");
 		// $('.nav-arrow').animate({ top: 305 }, {duration: 1000, easing: 'easeOutBounce'});
 		var welcomediv = $('.welcome-content');
 		var storydiv = $('.story-content');
+		var proposaldiv = $('.proposal-content');
 		var weddingdiv = $('.wedding-content');
 		var weddingheaderdiv = $('.wedding-header');
 		$(window).scroll(function(){
-			if($(window).scrollTop()>500){
+			if($(window).scrollTop()>1400){
 				welcomediv.fadeIn("slow");
 			}
 			else {
@@ -24,7 +49,7 @@ $(document).ready(function(){
 		});
 
 		$(window).scroll(function(){
-			if($(window).scrollTop()>700){
+			if($(window).scrollTop()>1650){
 				storydiv.fadeIn("slow");
 				// $( ".story-content" ).fadeIn("slow").animate({marginLeft: "50px"}, 500);
 
@@ -37,7 +62,17 @@ $(document).ready(function(){
 		});
 
 		$(window).scroll(function(){
-			if($(window).scrollTop()<1600){
+			if($(window).scrollTop()<2050){
+				proposaldiv.fadeOut("fast");
+			}
+			else {
+				proposaldiv.fadeIn("slow");
+			}
+			
+		});
+
+		$(window).scroll(function(){
+			if($(window).scrollTop()<2700){
 				weddingheaderdiv.fadeOut("fast");
 			}
 			else {
@@ -47,7 +82,7 @@ $(document).ready(function(){
 		});
 
 		$(window).scroll(function(){
-			if($(window).scrollTop()<1800){
+			if($(window).scrollTop()<2800){
 				// weddingdiv.hide(1000);
 				weddingdiv.fadeOut("800");
 				// weddingdiv.fadeOut({ duration: 700, queue: false }).slideUp(800);
